978bb31ca8fa3128e26a8676b26ca35322c70bd9
Mark Florisson <markflorisson88@gmail.com>
2013-07-29 22:29:23 +0100

    Remove unittest2 dependency

--- llvmmath/__init__.py
+++ llvmmath/__init__.py
@@ -4,13 +4,8 @@ from __future__ import print_function, division, absolute_import
 __version__ = '0.1'
 
 from os.path import dirname, abspath
-import sys
 import logging
-
-if sys.version_info[:2] < (2, 7):
-    import unittest2 as unittest
-else:
-    import unittest
+import unittest
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
@@ -21,6 +16,9 @@ from .build import have_llvm_asm, have_clang
 from .libs import get_default_math_lib, get_mathlib_so, get_llvm_mathlib
 from .libs import get_libm, get_openlibm
 
+from llvmmath.tests import (test_abi, test_build, test_libs, test_linking,
+                            test_parsesyms, test_symbols)
+
 # ______________________________________________________________________
 # llvmmath.test()
 
@@ -29,7 +27,13 @@ pattern = "test_*.py"
 
 def test(root=root, pattern=pattern):
     """Run tests and return exit status"""
-    tests =  unittest.TestLoader().discover(root, pattern=pattern)
+    # We can't use unittest's discover feature, since it's new in 2.7
+    # We can't have a dependency on unittest2
+    tests = []
+    loader = unittest.TestLoader()
+    for module in (test_abi, test_build, test_libs, test_linking,
+                   test_parsesyms, test_symbols):
+        tests.extend(loader.loadTestsFromModule(module))
     runner = unittest.TextTestRunner()
-    result = runner.run(tests)
+    result = runner.run(unittest.TestSuite(tests))
     return not result.wasSuccessful()
